15c551feadbeabc5694f8edcd81e395ee44c012c
Dmitriy Vorontsov <vorontsof@mail.ru>
2019-09-18 22:31:30 +0400

    basket applied everywhere

--- public/js/CartComp.js
+++ public/js/CartComp.js
@@ -71,34 +71,35 @@ Vue.component('cart', {
   template: `<div>
             <button class="btn-cart" type="button" @click="showCart = !showCart"><img class="cart-pic" src="img/cart.svg" alt="cart"></button>
             <div class="drop drop__cart" v-show="showCart">
-                <p v-if="!cartItems.length">Cart is empty</p>
+                <p class="cart-empty pink" v-if="!cartItems.length">Cart&nbsp;is&nbsp;empty</p>
                 <cart-item class="drop__cart_flex" 
                 v-for="item of cartItems" 
                 :key="item.id_product"
                 :cart-item="item" 
-                :img="imgCart"
+                :img="item.img"
                 @remove="remove">
                 </cart-item>
                 <div class="cart_flex-sum">
                      <p class="cart_flex-p fs16">total</p>
-                     <p class="cart_flex-p fs16">&#36;{{totalCart}}</p>
+                     <p class="cart_flex-p fs16">&#36;{{ totalCart }}</p>
                 </div>
                 <div class="drop__buttons">
                      <a href="checkout.html" class="dropmenu-button">checkout</a>
                      <a href="cart.html" class="dropmenu-button">go&nbsp;to&nbsp;cart</a>
                 </div>
             </div>
+            <div class="cart-counter">{{ totalAmount }}</div>
         </div>`
 });
 
 Vue.component('cart-item', {
   props: ['cartItem', 'img'],
   template: `<div class="drop__cart_flex">
-                 <img :src="img" alt="pic">
+                 <a href="item.html"><img class="cart__img" :src="img" alt="pic"></a>
                  <div class="drop__cart_flex-text">
-                     <a href="product.html" class="cart_flex-p">{{cartItem.product_name}}</a>
+                     <a href="product.html" class="cart_flex-p">{{ cartItem.product_name }}</a>
                      <a href="#"><img src="img/rating.png" style="padding: 10px 0;" alt="rating"></a>
-                     <p class="cart_flex-pink">{{cartItem.quantity}}&nbsp;x&nbsp;&#36;{{cartItem.price}}</p>
+                     <p class="cart_flex-pink">{{ cartItem.quantity }}&nbsp;x&nbsp;&#36;{{ cartItem.price }}</p>
                  </div>
                  <button class="del-btn" @click="$emit('remove', cartItem)"><i class="fas fa-times-circle"></i></button>
               </div>`
